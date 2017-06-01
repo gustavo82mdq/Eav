f37bc2757e0253364e3acebf98057454487976f0
gustavo82mdq <gustavo82mdq@gmail.com>
2017-06-01 12:41:08 -0300

    Travis tests

--- Test.php
+++ Test.php
@@ -5,8 +5,9 @@
  * Date: 01/06/17
  * Time: 12:01
  */
+require_once ('vendor/phpunit/phpunit/src/Framework/TestCase.php');
 
-class Test extends \PHPUnit_Framework_TestCase
+class Test extends PHPUnit_Framework_TestCase
 {
     public function testOnePlusOne() {
         $this->assertTrue(true);
