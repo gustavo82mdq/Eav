8afda8d49f91cbb2a900d271c0ecb3c97ed3d7ec
gustavo82mdq <gustavo82mdq@gmail.com>
2017-06-01 12:03:24 -0300

    Travis tests

---
+++ Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gus
+ * Date: 01/06/17
+ * Time: 12:01
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testOnePlusOne() {
+        $this->assertTrue(true);
+    }
+}
